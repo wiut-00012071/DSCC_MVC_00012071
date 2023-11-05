56591509e2828165ef183da3baba575947b6ec48
wiut-00012071 <wiut00012071@gmail.com>
2023-11-05 18:07:19 +0500

    Add project files.

---
+++ DSCC_MVC_00012071/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DSCC_MVC_00012071.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
